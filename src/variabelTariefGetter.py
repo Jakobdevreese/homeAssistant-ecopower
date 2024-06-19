2280127bc85fd35295cb5f02ad43b3e87603bad1
Jakob De Vreese <jakob.devreese@student.hogent.be>
2024-06-19 11:40:16 +0200

    aanzet variabel tarief berekenen

---
+++ src/variabelTariefGetter.py
@@ -0,0 +1,11 @@
+# ophalen van variabel tarief voor de maand
+
+rlp = 0 # moet een json dataset worden
+prijzen = 0 # moet een json dataset worden
+
+def getVariabelTarief():
+        
+    return 0.20 # dummie data
+
+
+
