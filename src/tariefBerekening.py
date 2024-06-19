42643ad95b7a9aa6c9b5f93ca8c9ebc0c60b7589
Jakob De Vreese <jakob.devreese@student.hogent.be>
2024-06-19 09:51:56 +0200

    aanpassingen tarfiefberekening

--- src/tariefBerekening.py
+++ src/tariefBerekening.py
@@ -3,9 +3,14 @@
 import vastTariefGetter
 
 # de variabelen
+verbruik = 0
+productie = 0
+maandpiek = 0
 vastTarief = vastTariefGetter.getAfnameTarief()
 terugleverTarief = vastTariefGetter.getTerugleverTarief()
 capaciteitsTarief = 0
+maximumTarief = 0
+minimumTarief = 0
 databeheerTarief = 0
 afnameTarief = 0
 energieBijdrageTarief = 0.0019261
@@ -22,12 +27,24 @@ def energiekosten(verbruik, productie, vastTarief, terugleverTarief):
 
     return vast - teruglever
 
-def netkosten(verbruik, maandpiek, capaciteitsTarief, databeheerTarief, afnameTarief):
+def netkosten(verbruik, maandpiek, capaciteitsTarief, databeheerTarief, afnameTarief, maximumTarief, minimumTarief):
+    # de totale netkosten hebben een maximum en minimum tarief
+    
+    # berekening van de netkosten
     capaciteit = (maandpiek * capaciteitsTarief) / 12
     databeheer = databeheerTarief / 12
     afname = verbruik * afnameTarief
 
+    # maximumtarief controleren
+    if ((capaciteit + databeheer + afname)/verbruik) > maximumTarief * verbruik:
+        return maximumTarief * verbruik
+    
+    # minimumtarief controleren
+    if capaciteit + databeheer + afname < minimumTarief:
+        return minimumTarief
+
     return capaciteit + databeheer + afname
+
 
 def heffingen(verbruik, energieBijdrageTarief, accijnzenTarief):
     bijdrage = verbruik * energieBijdrageTarief
@@ -35,8 +52,18 @@ def heffingen(verbruik, energieBijdrageTarief, accijnzenTarief):
 
     return bijdrage + accijnzen
 
+def bepaalAccijnzenTarief(verbruik):
+    if verbruik <= 3000:
+        return accijnzenTarief_0_3000
+    elif verbruik <= 20000:
+        return accijnzenTarief_3000_20000
+    elif verbruik <= 50000:
+        return accijnzenTarief_20000_50000
+    else:
+        return accijnzenTarief_50000
+
 # formule
-totalePrijsExcBtw = energieKosten(verbruik, productie, vastTarief, terugleverTarief) + netkosten(verbruik, maandpiek, capaciteitsTarief, databeheerTarief, afnameTarief) + heffingen(verbruik, energieBijdrageTarief, accijnzenTarief) 
+totalePrijsExcBtw = energiekosten(verbruik, productie, vastTarief, terugleverTarief) + netkosten(verbruik, maandpiek, capaciteitsTarief, databeheerTarief, afnameTarief, maximumTarief, minimumTarief) + heffingen(verbruik, energieBijdrageTarief, bepaalAccijnzenTarief(verbruik)) 
 totalePrijsIncBtw = totalePrijsExcBtw * (1 + btwTarief)
 
 
